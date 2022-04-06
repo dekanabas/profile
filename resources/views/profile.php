7226c2a0a0b5ed00ed87cf5291b7a2650db10a38
Your Name <meer.800454@spu.edu.iq>
2022-04-06 12:31:07 -0800

    first commit

---
+++ resources/views/profile.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="dekan.jpg" width="240.5" height="475.5">
+    <p>My name is Dekan Abas</p>
+    <p>I am student at IT department.</p>
+    <p> relationship: married.</p>
+    <p>Phone_number: 0773 058 1202. </p>
+</body>
+</html>
